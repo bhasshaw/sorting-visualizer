333718fdda50cc48f42c58169d6947d0ed99e48a
Bryce Hasshaw <bhasshaw@gmail.com>
2019-10-18 17:29:18 -0700

    added array

--- src/app/visualizer/visualizer.component.ts
+++ src/app/visualizer/visualizer.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./visualizer.component.scss']
 })
 export class VisualizerComponent implements OnInit {
+  array;
 
-  constructor() { }
+  constructor() {
+    this.array = [3, 5, 1, 3, 2, 8, 9, 6, 7, 10]
+  }
 
   ngOnInit() {
+    console.log(this.array)
   }
+
+
 
 }
