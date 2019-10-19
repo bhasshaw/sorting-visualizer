8b8c5a49431ff552fcfd2475803cbddff7effbb3
Bryce Hasshaw <bhasshaw@gmail.com>
2019-10-19 14:21:58 -0700

    rough draft for bubble sort

--- src/app/visualizer/visualizer.component.ts
+++ src/app/visualizer/visualizer.component.ts
@@ -6,16 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./visualizer.component.scss']
 })
 export class VisualizerComponent implements OnInit {
-  array;
+  array
+  show:boolean
 
   constructor() {
     this.array = [3, 5, 1, 3, 2, 8, 9, 6, 7, 10]
+    // this.arrayRandom = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
+    // this.arrayOrdered = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    // this.arrayReversed = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
   }
 
   ngOnInit() {
-    console.log(this.array)
+    // this.bubbleSort()
   }
 
-
+  bubbleSort() {
+    console.log('bubblesort')
+    let swapp
+    let n = this.array.length - 1
+    let x = this.array;
+    this.show = false;
+    do {
+      swapp = false;
+      for (let i = 0; i < n; i++) {
+        if (x[i] > x[i + 1]) {
+          let temp = x[i]
+          x[i] = x[i + 1]
+          x[i + 1] = temp
+          swapp = true
+        }
+      }
+      n--
+    } while (swapp)
+    this.show = true;
+    console.log(this.array)
+    return x
+  }
 
 }
